389af9d37a50d29ccaad043b411bb29139548697
eugenleo048 <eugenleo048@gmail.com>
2025-08-31 12:32:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 
 # Токен берём из переменной окружения (безопасно!)
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("8257985518:AAGlJSJb4LtwUZ_tGlUem1UERC1jbtIeOvg")
 
 # Ссылка на твой GitHub Pages сайт с radio.html
 WEBAPP_URL = "https://yourusername.github.io/telegram-radio/radio.html"
